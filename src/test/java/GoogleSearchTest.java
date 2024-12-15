24a096d6a8b1033f527dafc26286a109da8609f4
Danyal Zahid <danyal.zahid2000@gmail.com>
2024-12-15 22:52:18 +0500

    complete

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -5,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class GoogleSearchTest {
 
@@ -20,15 +23,18 @@ public class GoogleSearchTest {
     @Test
     public void searchGoogle() {
         // Open Google
-        driver.get("https://www.google.com");
+        driver.get("https://www.saucedemo.com/v1/");
         
         // Find the search box, input a query, and submit
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium WebDriver");
-        searchBox.submit();
-        
-        // Print page title
-        System.out.println("Page title is: " + driver.getTitle());
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.sendKeys("standard_user");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("secret_sauce");
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inventory_filter_container")));
     }
 
     @AfterClass
